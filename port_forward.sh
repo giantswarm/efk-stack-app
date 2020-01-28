938f7e62134c1e4be49e7adfeb61ab835787483d
Pau Rosello <paurosello@gmail.com>
2020-01-28 19:12:31 +0100

    Add fluentd

--- port_forward.sh
+++ port_forward.sh
@@ -3,5 +3,3 @@
 POD=$(kubectl get pod -n opendistro -l app=opendistro-opendistro-es-kibana -o jsonpath="{.items[0].metadata.name}")
 
 kubectl port-forward $POD 5601:5601 -n opendistro
-
-xdg-open https://127.0.0.1:5601
