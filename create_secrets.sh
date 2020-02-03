a8ef76ed4592a3f27437d9920807031d0c296d1a
Pau Rosello <paurosello@gmail.com>
2020-02-03 16:42:59 +0100

    namespace and cronjob for certs

--- create_secrets.sh
+++ create_secrets.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-kubectl create ns opendistro2
+# kubectl create ns opendistro
 
-kubectl create secret generic kibana-auth --from-literal=username='admin' --from-literal=password='admin'  --from-literal=cookie='akrjp45dpluix6vlbbivdv3e0w03fkichp29llkr' -n opendistro2
+# kubectl create secret generic kibana-auth --from-literal=username='admin' --from-literal=password='admin'  --from-literal=cookie='akrjp45dpluix6vlbbivdv3e0w03fkichp29llkr' -n opendistro
 
-kubectl create secret generic elasticsearch-transport-certs -n opendistro2 \
-      --from-file=elk-transport-crt.pem=./out/transport.crt \
-      --from-file=elk-transport-key.pem=./out/transport.pem \
-      --from-file=elk-transport-root-ca.pem=./out/root.crt
+# kubectl create secret generic elasticsearch-transport-certs -n opendistro \
+#       --from-file=elk-transport-crt.pem=./out/transport.crt \
+#       --from-file=elk-transport-key.pem=./out/transport.pem \
+#       --from-file=elk-transport-root-ca.pem=./out/root.crt
 
-kubectl create secret generic -n opendistro2 elasticsearch-security-config --from-file=configs/config.yml
+# kubectl create secret generic -n opendistro elasticsearch-security-config --from-file=configs/config.yml
--- manual_deploy.sh
+++ manual_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kubectl create ns opendistro2
+kubectl create ns opendistro
 
-helm template --namespace opendistro2 --name opendistro --tiller-namespace giantswarm ./helm/opendistro-app > deploy.yaml
+helm template --namespace opendistro --name opendistro --tiller-namespace giantswarm ./helm/opendistro-app > deploy.yaml
 
 kubectl apply -f deploy.yaml
