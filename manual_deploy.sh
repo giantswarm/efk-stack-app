ed421752f27ec5b1ed57a682c8b642cc4714cc2b
Pau Rosello <paurosello@gmail.com>
2020-02-07 16:35:33 +0100

    rename chart

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -2,6 +2,6 @@
 
 kubectl create ns opendistro
 
-helm template --namespace opendistro --name opendistro --tiller-namespace giantswarm ./helm/elastic-stack-app -f example_values/ingress_enabled.yaml > deploy.yaml
+helm template --namespace opendistro --name opendistro --tiller-namespace giantswarm ./helm/efk-stack-app -f example_values/ingress_enabled.yaml > deploy.yaml
 
 kubectl apply -f deploy.yaml
