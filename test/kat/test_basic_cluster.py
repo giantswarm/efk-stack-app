19bb12f72fd3cf2ec9e95130cf2496f5c81b537a
Tobias Bradtke <webwurst@gmail.com>
2020-10-12 15:14:42 +0200

    Add pytest example

---
+++ test/kat/test_basic_cluster.py
@@ -0,0 +1,32 @@
+"""This module shows some very basic examples of how to use fixtures in pytest-helm-charts.
+"""
+import logging
+from typing import Dict
+
+import pykube
+
+from pytest_helm_charts.fixtures import Cluster
+
+logger = logging.getLogger(__name__)
+
+
+def test_api_working(kube_cluster: Cluster) -> None:
+    """Very minimalistic example of using the [kube_cluster](pytest_helm_charts.fixtures.kube_cluster)
+    fixture to get an instance of [Cluster](pytest_helm_charts.clusters.Cluster) under test
+    and access its [kube_client](pytest_helm_charts.clusters.Cluster.kube_client) property
+    to get access to Kubernetes API of cluster under test.
+    Please refer to [pykube](https://pykube.readthedocs.io/en/latest/api/pykube.html) to get docs
+    for [HTTPClient](https://pykube.readthedocs.io/en/latest/api/pykube.html#pykube.http.HTTPClient).
+    """
+    assert kube_cluster.kube_client is not None
+    assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
+
+
+def test_cluster_info(kube_cluster: Cluster, cluster_type: str, chart_extra_info: Dict[str, str]) -> None:
+    """Example shows how you can access additional information about the cluster the tests are running on"""
+    logger.info(f"Running on cluster type {cluster_type}")
+    key = "external_cluster_type"
+    if key in chart_extra_info:
+        logger.info(f"{key} is {chart_extra_info[key]}")
+    assert kube_cluster.kube_client is not None
+    assert cluster_type != ""
